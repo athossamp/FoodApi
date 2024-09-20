2cafcd5d5aebe2e44002261d46311449dbde4e8d
athossamp <148923585+athossamp@users.noreply.github.com>
2024-09-20 10:50:17 -0300

    Update Settings.cs

--- Settings.cs
+++ Settings.cs
@@ -2,7 +2,7 @@
 {
     public static class Settings
     {
-        //public static string Secret = "fedaf7d8863b48e197b9287d492b708e";
-        public static string Secret = "fedaf7d8863b48e197b9287d492b708e";
+      
+        public static string Secret = "";
     }
 }
